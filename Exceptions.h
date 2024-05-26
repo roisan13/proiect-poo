bb0da9eacd812dd68f8960b9249eeb6baa2aaf4e
roisan13 <raducanu13@gmail.com>
2024-05-27 02:56:39 +0300

    Tema 2

--- Exceptions.h
+++ Exceptions.h
@@ -14,7 +14,7 @@ class gameError : public std::runtime_error {
 class attributeError : public gameError {
 public:
     explicit attributeError(const std::string &where) :
-            gameError("Incorrect/invalid attribute given as parameter in constructor" + where) {}
+            gameError(std::string("Incorrect/invalid attribute given as parameter in constructor") + where) {}
 };
 
 class gameLogicError : public gameError {
